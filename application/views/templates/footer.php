c892ab162ad6a67d6f7e3d641cc0190d768e19c3
zobeltran <renzoe.beltran@gmail.com>
2017-11-19 13:47:32 +0800

    Added footer and header templates

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+
+    <!-- Javascript -->
+    <script src="<?=base_url();?>assets/js/jquery-1.11.1.min.js"></script>
+    <script src="<?=base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?=base_url();?>assets/js/scripts.js"></script> 
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aimex Software</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+    <link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/form-elements.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/style.css">
+    <!-- Favicon and touch icons -->
+    <link rel="shortcut icon" href="<?=base_url();?>assets/ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=base_url();?>assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=base_url();?>assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=base_url();?>assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?=base_url();?>assets/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<body>
