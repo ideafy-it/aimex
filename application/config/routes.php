4e70039b2372c56a158be4c377a4c0b82e870373
zobeltran <renzoe.beltran@gmail.com>
2017-11-19 18:50:31 +0800

    Changed Clients to Admins & added Homes as default controller #8

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'welcome';
-$route['clients/(:any)'] = 'clients/view/$1';
+$route['default_controller'] = 'homes';
+$route['(:any)'] = 'homes/view/$1';
+$route['admins/(:any)'] = 'admins/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
