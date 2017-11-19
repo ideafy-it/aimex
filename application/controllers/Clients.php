22bdd51616058e000e79076ebacf80b03ea1e4fe
zobeltran <renzoe.beltran@gmail.com>
2017-11-19 13:46:06 +0800

    Added Clients controller

---
+++ application/controllers/Clients.php
@@ -0,0 +1,15 @@
+<?php
+class Clients extends CI_Controller {
+    public function view($client = 'home'){
+        if(!file_exists(APPPATH.'views/clients/'.$client.'.php')){
+            // Shows error if page does not exist
+            show_404();
+        }
+
+        $data['title'] =  ucfirst($client); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('clients/'.$client, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
