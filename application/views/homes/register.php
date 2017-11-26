2082260cd4976f4815f8e36905a27b7bf16d28d9
zobeltran <rnzbeltran@gmail.com>
2017-11-27 02:21:38 +0800

    Fixed html structure of register

--- application/views/homes/register.php
+++ application/views/homes/register.php
@@ -1,74 +1,74 @@
-        <!-- Top content -->
-         <div class="top-content">
-         <nav class="navbar navbar-toggleable-md navbar-light bg-faded">                
-                  <a class="navbar-brand" href="#">AIMEX SOFTWARE</a>
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                      <li class="nav-item">
-                        <a class="nav-link" href="index.html">LOGIN</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="register.html">REGISTER</a>
-                      </li>
-                    </ul>
-                  </div>
-                </nav>
-
-            <div class="inner-bg">
-                <div class="container">
-                 <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1 id="h1-login-form"><strong>AIMEX</strong> USER REGISTRATION</h1>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3 form-box">
-                        	<div class="form-top">
-                        		<div class="form-top-left">
-                                    <h3>REGISTER AN ACCOUNT HERE</h3>
-                        			<p>ENTER EMPLOYEE DETAILS TO ENROLL:</p>
-                        		</div>
-                        		<div class="form-top-right">
-                        			<i class="fa fa-lock"></i>
-                        		</div>
-                            </div>
-                            <div class="form-bottom">
-			                    <form role="form" action="" method="post" class="login-form">
-			                    	<div class="form-group">
-			                    		<label class="sr-only" for="form-username">Employee Name</label>
-			                        	<input type="text" name="form-username" placeholder="EMPLOYEE NAME" class="form-username form-control" id="form-username">
-			                        </div>
-			                        <div class="form-group">
-			                        	<label class="sr-only" for="form-password">Username</label>
-			                        	<input type="password" name="form-password" placeholder="USERNAME" class="form-password form-control" id="form-password">
-			                        </div>
-                                    <div class="form-group">
-                                        <label class="sr-only" for="form-password">Password</label>
-                                        <input type="password" name="form-password" placeholder="PASSWORD" class="form-password form-control" id="form-password">
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="sr-only" for="form-password">Confirm Password</label>
-                                        <input type="password" name="form-password" placeholder="CONFIRM PASSWORD" class="form-password form-control" id="form-password">
-                                    </div>
-                                    
-                                    <div class="form-group">
-                                    <div class="dropdown">
-                                      <button class="dropbtn">CHOOSE EMPLOYEE ROLE HERE</button>
-                                      <div class="dropdown-content">
-                                            <a href="#">LOAN PROCESSOR</a>
-                                            <a href="#">LOAN COLLECTOR</a>
-                                         </div>
-                                        </div>
-                                        
-                                        <br><br>
-                                        <button type="submit" class="btn-enroll">ENROLL USER</button>
-                                
-                                     </div>
-			                    </form>
-		                    </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
+<!-- Top content -->
+<div class="top-content">
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">                
+        <a class="navbar-brand" href="#">AIMEX SOFTWARE</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">LOGIN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.html">REGISTER</a>
+                </li>
+            </ul>
         </div>
+    </nav>
+
+   <div class="inner-bg">
+       <div class="container">
+        <div class="row">
+               <div class="col-sm-8 col-sm-offset-2 text">
+                   <h1 id="h1-login-form"><strong>AIMEX</strong> USER REGISTRATION</h1>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-sm-6 col-sm-offset-3 form-box">
+               	<div class="form-top">
+               		<div class="form-top-left">
+                           <h3>REGISTER AN ACCOUNT HERE</h3>
+               			<p>ENTER EMPLOYEE DETAILS TO ENROLL:</p>
+               		</div>
+               		<div class="form-top-right">
+               			<i class="fa fa-lock"></i>
+               		</div>
+                   </div>
+                   <div class="form-bottom">
+              <form role="form" action="" method="post" class="login-form">
+              	<div class="form-group">
+              		<label class="sr-only" for="form-username">Employee Name</label>
+                  	<input type="text" name="form-username" placeholder="EMPLOYEE NAME" class="form-username form-control" id="form-username">
+                  </div>
+                  <div class="form-group">
+                  	<label class="sr-only" for="form-passusername">Username</label>
+                  	<input type="password" name="form-passusername" placeholder="USERNAME" class="form-username form-control" id="form-username">
+                  </div>
+                           <div class="form-group">
+                               <label class="sr-only" for="form-password">Password</label>
+                               <input type="password" name="form-password" placeholder="PASSWORD" class="form-password form-control" id="form-password">
+                           </div>
+                           <div class="form-group">
+                               <label class="sr-only" for="form-password">Confirm Password</label>
+                               <input type="password" name="form-password" placeholder="CONFIRM PASSWORD" class="form-password form-control" id="form-password">
+                           </div>
+                           
+                           <div class="form-group">
+                           <div class="dropdown">
+                             <button class="dropbtn">CHOOSE EMPLOYEE ROLE HERE</button>
+                             <div class="dropdown-content">
+                                   <a href="#">LOAN PROCESSOR</a>
+                                   <a href="#">LOAN COLLECTOR</a>
+                                </div>
+                               </div>
+                               
+                               <br><br>
+                               <button type="submit" class="btn-enroll">ENROLL USER</button>
+                       
+                            </div>
+              </form>
+             </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   
+</div>
