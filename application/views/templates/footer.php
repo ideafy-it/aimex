ab70af44ad757735cc85eab0fd75a47dfdb768e2
zobeltran <rnzbeltran@gmail.com>
2017-11-27 01:24:25 +0800

    Added parsley.min.js to footer scripts

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,5 +3,6 @@
     <script src="<?=base_url();?>assets/js/jquery-1.11.1.min.js"></script>
     <script src="<?=base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?=base_url();?>assets/js/scripts.js"></script> 
+    <script src="<?=base_url();?>assets/js/parsley.min.js"></script>
 </body>
 </html>
