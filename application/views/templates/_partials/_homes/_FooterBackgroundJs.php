0f9d1a728c0d79106148ae1d898cae429e5eb1be
zobeltran <rnzbeltran@gmail.com>
2017-11-29 09:51:51 +0800

    Added parsley.min.js

--- application/views/templates/_partials/_homes/_FooterBackgroundJs.php
+++ application/views/templates/_partials/_homes/_FooterBackgroundJs.php
@@ -3,3 +3,4 @@
     <script src="<?=base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?=base_url();?>assets/js/jquery.backstretch.min.js"></script>
     <script src="<?=base_url();?>assets/js/scripts.js"></script> 
+    <script src="<?=base_url();?>assets/js/parsley.min.js"></script>
